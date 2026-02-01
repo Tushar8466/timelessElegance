b47b608e22bb35db449b298bb630856aae8e28bb
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-01 15:48:04 +0530

    feat: Implement responsive mobile navigation with a full-screen menu and add styles for new watch product and brand display sections.

---
+++ script.js
@@ -0,0 +1,120 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline();
+
+tl.from(".logo", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out"
+})
+tl.from(".nav-links a", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.2
+})
+tl.from(".icon", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.2
+})
+tl.from(".hero-content h1", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out"
+})
+tl.from(".hero-content p", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out"
+})
+tl.from(".btns", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out"
+})
+gsap.from(".hero-content", {
+    opacity: 0,
+    y: -20,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".hero-content",
+        start: "top 80%",
+        toggleActions: "play none none none"
+    }
+})
+
+gsap.from(".watch-section p", {
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.2,
+    delay: 0.3,
+    scrollTrigger: {
+        trigger: ".watch-section",
+        scroller: "body",
+        start: "top 80%",
+        toggleActions: "play none none none"
+    }
+});
+gsap.from(".watch-section h2", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.3,
+    ease: "power2.out",
+    scroller: "body",
+    stagger: 0.2,
+    scrollTrigger: {
+        trigger: ".watch-section",
+        start: "top 80%",
+        toggleActions: "play none none none"
+    }
+});
+
+let menuBtn = document.querySelector("#nav-menu");
+let closeBtn = document.querySelector("#close-menu");
+
+let menuTl = gsap.timeline({ paused: true });
+
+menuTl.to("#full-menu", {
+    right: 0,
+    duration: 0.6,
+    ease: "power3.out"
+});
+
+menuTl.to(".menu-links a", {
+    opacity: 1,
+    x: 0,
+    stagger: 0.1,
+    duration: 0.4,
+    ease: "power2.out"
+});
+
+menuBtn.addEventListener("click", () => {
+    menuTl.play();
+});
+
+closeBtn.addEventListener("click", () => {
+    menuTl.reverse();
+});
+
+gsap.from(".watch-brand-card", {
+    opacity: 0,
+    y: 30,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.1,
+    scrollTrigger: {
+        trigger: ".watch-brands",
+        start: "top 80%",
+        toggleActions: "play none none none"
+    }
+});
